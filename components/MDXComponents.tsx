e54098d217cc4d2ef8206af65e92f6bda73f5d32
Ousinki <ousinki@outlook.com>
2025-12-24 21:35:21 +0800

    fix: register chart components in MDXComponents

--- components/MDXComponents.tsx
+++ components/MDXComponents.tsx
@@ -5,6 +5,7 @@ import type { MDXComponents } from 'mdx/types'
 import Image from './Image'
 import CustomLink from './Link'
 import TableWrapper from './TableWrapper'
+import { SociologyBarChart, SociologyLineChart, SociologyPieChart } from './charts'
 
 export const components: MDXComponents = {
   Image,
@@ -13,4 +14,8 @@ export const components: MDXComponents = {
   pre: Pre,
   table: TableWrapper,
   BlogNewsletterForm,
+  SociologyBarChart,
+  SociologyLineChart,
+  SociologyPieChart,
 }
+
