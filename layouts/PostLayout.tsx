7ff3c02bb09eb0b8289f2a1b36af5a01d6096d51
Ousinki <ousinki@outlook.com>
2025-12-25 11:13:42 +0800

    fix: prettier formatting in PostLayout

--- layouts/PostLayout.tsx
+++ layouts/PostLayout.tsx
@@ -107,9 +107,7 @@ export default function PostLayout({ content, authorDetails, next, prev, childre
                 </div>
               )}
               {/* Table of Contents */}
-              {toc && Array.isArray(toc) && toc.length > 0 && (
-                <TOCSidebar toc={toc} />
-              )}
+              {toc && Array.isArray(toc) && toc.length > 0 && <TOCSidebar toc={toc} />}
             </dl>
             <div className="divide-y divide-gray-200 xl:col-span-3 xl:row-span-2 xl:pb-0 dark:divide-gray-700">
               <div className="prose dark:prose-invert max-w-none pt-10 pb-8">{children}</div>
